251735058fe851755490fe056cdf6be0a1757ab0
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-09-05 10:17:41 -0500

    Recepcion de Parametros

--- Parcial1/Parametros/Routes/Routes.js
+++ Parcial1/Parametros/Routes/Routes.js
@@ -8,8 +8,26 @@ export const crearRouter = () => {
         res.json({ mensaje: "CORS == Mi Diversion nomas" });
     });
 
-    nuevoRouter.post("/Ver", Verificacion, (req, res) => {
+    nuevoRouter.post("/VerBody", Verificacion, (req, res) => {
         res.json({ mensaje: "Verificacion Completada" });
+    });
+
+    nuevoRouter.post("/VerParam/:id", (req, res) => {
+        res.json({
+            mensaje:
+                "Verificacion Completada con Parametros con ID: " +
+                req.params.id,
+        });
+    });
+
+    nuevoRouter.post("/VerQuery", (req, res) => {
+        res.json({
+            mensaje:
+                "Verificacion Completada con Query: " +
+                req.query.nombre +
+                " " +
+                req.query.apellido,
+        });
     });
 
     return nuevoRouter;

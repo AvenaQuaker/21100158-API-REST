af2d04d1f6ec911dbe00502a1d57c97385f3bed2
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-09-11 09:20:39 -0500

    Controladores

--- Parcial1/ExpressBD/Routes/Routes.js
+++ Parcial1/ExpressBD/Routes/Routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { MovieController } from "../Controllers/mySQL.js";
+import { HomeController } from "../Controllers/Home.js";
 
 export const crearRouter = () => {
     const nuevoRouter = new Router();
     const movieController = new MovieController();
+    const homeController = new HomeController();
 
+    nuevoRouter.get("/", homeController.Home);
     nuevoRouter.get("/movies", movieController.General);
     nuevoRouter.get("/movies/:id", movieController.Buscar);
 

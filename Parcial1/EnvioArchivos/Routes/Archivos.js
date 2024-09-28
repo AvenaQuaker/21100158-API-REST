f7fed14ddfcd4a5d83c74cf6d45ff6b5415f8947
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-09-28 12:25:03 -0500

    Prueba React

---
+++ Parcial1/EnvioArchivos/Routes/Archivos.js
@@ -0,0 +1,67 @@
+import { Router } from "express";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./Parcial1/FormularioArchivos/Uploads/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+
+// Obtener el directorio base correctamente
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Router de Archivos
+export const crearRouterARCH = () => {
+    const nuevoRouter = new Router();
+
+    nuevoRouter.post("/Enviar", upload.single("archivo"), (req, res) => {
+        console.log(req.file);
+    });
+
+    nuevoRouter.get("/Recibir", (req, res) => {
+        const filePath = path.join(__dirname, "../Uploads", "porta.png");
+        res.sendFile(filePath, (err) => {
+            if (err) {
+                console.error("Error al enviar el archivo:", err);
+            }
+        });
+    });
+
+    nuevoRouter.get("/sendfile", (req, res) => {
+        const filePath = path.join(__dirname, "../Uploads", "porta.png");
+        res.sendFile(filePath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    nuevoRouter.get("/download", (req, res) => {
+        const filePath = path.join(__dirname, "../Uploads", "porta.png");
+        res.download(filePath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+
+    nuevoRouter.get("/attachment", (req, res) => {
+        const filePath = path.join(__dirname, "../Uploads", "porta.png");
+        res.attachment(filePath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+        res.send();
+    });
+
+    return nuevoRouter;
+};
